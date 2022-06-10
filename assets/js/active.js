1e70dfc694ea10a2e2a54a24e44dd4c9d42656a6
Anshu Chauhan <101622044+ac51019@users.noreply.github.com>
2022-06-10 18:30:49 +0530

    Add files via upload

---
+++ assets/js/active.js
@@ -0,0 +1,45 @@
+(function($) {
+    'use strict';
+
+    $('.html').rProgressbar({
+        percentage: 80,
+        fillBackgroundColor: '#1A919A'
+    });
+    $('.css').rProgressbar({
+        percentage: 80,
+        fillBackgroundColor: '#1A919A'
+    });
+
+
+
+    $('.bootstrap').rProgressbar({
+        percentage: 75,
+        fillBackgroundColor: '#1A919A'
+    });
+
+
+
+    $('.javascript').rProgressbar({
+        percentage: 40,
+        fillBackgroundColor: '#1A919A'
+    });
+
+
+
+    $('.jquery').rProgressbar({
+        percentage: 30,
+        fillBackgroundColor: '#1A919A',
+    });
+
+    $('.react').rProgressbar({
+        percentage: 60,
+        fillBackgroundColor: '#1A919A',
+    });
+
+
+
+
+
+
+
+})(jQuery);
